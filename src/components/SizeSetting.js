89f814bd5de2102110b70e9383643430a1518d2c
Accel <cyberpunk2076@gmail.com>
2020-10-24 00:51:37 +0700

    change a bit jsx

--- src/components/SizeSetting.js
+++ src/components/SizeSetting.js
@@ -8,18 +8,18 @@ export default class SizeSetting extends Component {
         <div className="card-body">
           <div className="btn-group" role="group" aria-label="Basic example">
             <button
+              className="btn btn-primary mr-1"
+              type="button"
+              onClick={() => this.props.changeSize(-2)}
+            >
+              Decrease
+            </button>
+            <button
               className="btn btn-primary"
               type="button"
               onClick={() => this.props.changeSize(2)}
             >
               Increase
-            </button>
-            <button
-              className="btn btn-primary ml-1"
-              type="button"
-              onClick={() => this.props.changeSize(-2)}
-            >
-              Decrease
             </button>
           </div>
         </div>
